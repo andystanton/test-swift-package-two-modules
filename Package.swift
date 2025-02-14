e8a171079988e211e20505e68a3299294b3113ae
Andy Stanton <andystanton@users.noreply.github.com>
2025-02-14 22:26:56 +0000

    Initial commit

--- Package.swift
+++ Package.swift
@@ -19,7 +19,8 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "test-swift-package-module-1"),
+            name: "test-swift-package-module-1",
+            dependencies: ["test-swift-package-module-2"]),
         .target(
             name: "test-swift-package-module-2"),
     ]
